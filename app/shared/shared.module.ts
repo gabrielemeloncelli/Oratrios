7d08a29f1199a0497ac589cbcae5a370a4b0afe2
gabrielemeloncelli <gabriele.b.meloncelli@gmail.com>
2017-05-16 08:43:59 +0200

    Factor top bar as a component

--- app/shared/shared.module.ts
+++ app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule }     from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule }  from '@angular/forms';
 
-import { LoginComponent } from './login.component';
+import { LoginComponent }   from './login.component';
+import { TopBarComponent }  from './top-bar.component';
 
 
 @NgModule({
   imports: [ CommonModule, FormsModule ],
-  declarations: [ LoginComponent ],
-  exports: [ LoginComponent ]
+  declarations: [ LoginComponent, TopBarComponent ],
+  exports: [ LoginComponent, TopBarComponent ]
 })
 export class SharedModule {  }
---
+++ app/shared/top-bar.component.ts
@@ -0,0 +1,32 @@
+import { Component,
+            OnInit }        from '@angular/core';
+import { Validators,
+            FormGroup,
+            FormControl }   from '@angular/forms';
+
+import { UiStatusService }  from '../core/ui-status.service';
+
+
+@Component({
+  selector: 'top-bar',
+  templateUrl: 'app/shared/top-bar.component.html',
+
+
+})
+export class TopBarComponent implements OnInit {
+
+  constructor(private uiStatusService: UiStatusService) {       
+  }
+
+
+
+
+
+
+
+  ngOnInit() {
+  }
+
+
+
+}
